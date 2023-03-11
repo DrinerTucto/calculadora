e79b2602d04a4a758c726bd0ee752475f593a032
DrinerTucto <1304745@senati.pe>
2023-03-10 21:04:07 -0500

    calculadora

---
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val n1 = findViewById<TextView>(R.id.textView)
+        val bt1=findViewById<Button>(R.id.button2)
+        val bt2=findViewById<Button>(R.id.button4)
+        val bt3=findViewById<Button>(R.id.button2)
+        val bt4=findViewById<Button>(R.id.button)
+
+
+
+
+    }
+}
